7b268d8babdae6fe98a24fd4f696d6835c31fa42
lucasalmeida30 <lucas_almeida12@icloud.com>
2022-11-09 18:00:54 -0300

    arrumando branchs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <input type="text" />
-      <h3
-        data-testid="home-initial-message"
-      >
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </h3>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="text"
+          data-testid="query-input"
+        />
+
+        <button
+          type="button"
+          data-testid="query-button"
+        >
+          Pesquisar
+        </button>
+        <h3
+          data-testid="home-initial-message"
+        >
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h3>
+      </div>
+    );
+  }
 }
 
 export default App;
