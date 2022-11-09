aff28f657060ea225141dc48c4ef16614c2d5b15
Leocarlota <leonardo.carlota@gmail.com>
2022-11-09 20:18:35 -0300

    Requisito 04

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { getCategories } from '../services/api';
 
 class Home extends Component {
+  state = {
+    categories: [],
+  };
+
+  async componentDidMount() {
+    const categories = await getCategories();
+    this.setState({ categories });
+  }
+
   handleClick = () => {
     const { history } = this.props;
     history.push('/cart');
   };
 
   render() {
+    const { categories } = this.state;
     return (
       <div className="App">
+        {
+          categories.map((category) => (
+            <label
+              htmlFor="categorieRadioButton"
+              key={ category.id }
+              data-testid="category"
+            >
+              <input
+                type="radio"
+                name="button"
+                id="categorieRadioButton"
+              />
+              { category.name }
+            </label>
+          ))
+        }
         <input type="text" />
         <button
           type="button"
